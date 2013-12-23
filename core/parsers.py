6e7dd39a7291b5add77b49a0028a9d4f32498d42
FilipSkalski <fs237772@students.mimuw.edu.pl>
2013-12-23 04:16:49 +0100

    redone parsers

---
+++ core/parsers.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+from abc import ABCMeta, abstractmethod
+import os
+import sys
+from entities import Job, User
+
+
+def get_parser(filename):
+	"""
+	Return a parser based on the file extension.
+	"""
+	ext = os.path.splitext(filename)[1]
+	if ext == '.swf':
+		return SWFParser()
+	elif ext == '.sql':
+		return ICMParser()
+	else:
+		print "ERROR: No parser found for the extension", ext
+		sys.exit(1)
+
+
+class BaseParser(object):
+	"""
+	Base class for reading and parsing different workload files.
+	"""
+
+	__metaclass__ = ABCMeta
+
+	def parse_workload(self, filename, serial):
+		"""
+		A wrapper around the main parse method overloaded in subclasses.
+
+		Args:
+		  filename: path to some workload file.
+		  serial: change parallel jobs to multiple serial ones.
+
+		Returns:
+		  a list of `Jobs` already linked to `User` instances.
+		  a dictionary of created `User` instances.
+		"""
+		self.jobs = []
+		self.users = {}
+
+		f = open(filename)
+		for line in f:
+			stats = self._parse(line)
+			if stats is not None:
+				if stats['run_time'] <= 0 or stats['proc'] <= 0:
+					continue  # skip incomplete data
+				if serial:
+					count = stats['proc']
+					stats['proc'] = 1
+				else:
+					count = 1
+				for i in range(count):
+					self._next_job(stats)
+		f.close()
+		return self.jobs, self.users
+
+	def _next_job(self, stats):
+		"""
+		Add the job to the result list.
+		Replace the user id with a `User` instance.
+		"""
+		uid = stats['user']
+		if uid not in self.users:
+			self.users[uid] = User(uid)
+		self.jobs.append( Job(stats, self.users[uid]) )
+
+	@abstractmethod
+	def _parse(self, line):
+		"""
+		Parse a single line from the workload file.
+
+		Return a dictionary with job statistics or `None`
+		if that line did not contain job information.
+		"""
+		raise NotImplemented
+
+
+class SWFParser(BaseParser):
+	"""
+	Parser for the .swf workload files.
+	"""
+
+	# field numbers
+	job_id = 0
+	submit = 1
+	wait_time = 2
+	run_time = 3
+	proc = 4
+	user_id = 11
+	partition = 15
+
+	def _parse(self, line):
+		if line[0] == ';':
+			return  # skip comments
+		stats = map(int, line.split())
+		return {
+			'id': stats[self.job_id],
+			'user': stats[self.user_id],
+			'proc': stats[self.proc],
+			'submit': stats[self.submit],
+			'run_time': stats[self.run_time],
+		}
+
+
+class ICMParser(BaseParser):
+	"""
+	Parser for the ICM workload dump.
+	"""
+
+	# field numbers
+
+	def _parse(self, line):
+		raise NotImplemented
+		#TODO
--- main.py
+++ main.py
@@ -3,23 +3,24 @@
 import argparse
 import sys
 import time
-import parts.settings
-#from reader import SWFReader, ICMReader #TODO ZMIENIC IMPORT READOW NA TYLKO MODULE??
-
+from core import parsers, simulation
+from parts import settings
 
 
 def divide_jobs(jobs, first_job, block_time, block_margin):
 	"""
 	Divide the jobs into potentially many smaller blocks.
-	Each block can have extra jobs to fill up the cluster.
-	IN:
-	- jobs - list of all the jobs
-	- first_job - ID of the first job to start with
-	- block_time - length of each block in seconds or None
-	- block_margin - extra length added to the block on both sides
-	OUT:
-	- a list of consecutive blocks - a block is a dict of indexes
-	{left [margin start]:first [block job]:last [block job]:right [margin end]}
+	Each block can have extra jobs to fill up and empty the cluster.
+
+	Args:
+	  jobs: list of all the jobs.
+	  first_job: ID of the first job to start with.
+	  block_time: length of each block in seconds or ``zero``.
+	  block_margin: extra length added to the block on both sides.
+
+	Returns:
+	  a list of consecutive blocks - a block is a dict of indexes
+	    {left [margin start]:first [block job]:last [block job]:right [margin end]}
 	"""
 	for i, j in enumerate(jobs):
 		if j.ID == first_job:
@@ -63,13 +64,16 @@ def divide_jobs(jobs, first_job, block_time, block_margin):
 def run(args):
 	"""
 	"""
+	#TODO ZMIENILY SIE PARAMS: TERAZ 0 == NONE
 	print "RUNWWW", args
 	return 0
-	#TODO select reader based on extenstion
-#TODO DODAC TIME.CTIME DO TITLE! default=time.ctime(),
-	reader = SWFReader()
-	jobs, users = reader.parse_workload(args['workload'], args['serial'])
-	jobs.sort(key=lambda j: j.submit) # order by submit time
+
+
+
+	parser = parsers.get_parser(args['workload'])
+
+	jobs, users = parser.parse_workload(args['workload'], args['serial'])
+	jobs.sort(key=lambda j: j.submit)  # order by submit time
 
 	for j in jobs:
 		# TODO ADD TIME LIMIT
@@ -88,7 +92,7 @@ def run(args):
 
 	blocks = divide_jobs(jobs, args['job_id'], block_time, block_margin)
 #TODO all users -> do symulacji tylko tych ktorzy tam wystepuja + reset
-
+#TODO DODAC TIME.CTIME DO TITLE! default=time.ctime(),
 	for b in blocks:
 		full_slice = jobs[b['left']:b['right']+1] # block includes both ends
 
@@ -128,13 +132,12 @@ def config(args):
 		if temp.loc is not None:
 			print '# Used by `{}`'.format(temp.loc)
 
-	#TODO RECREATE
 	print '##\n## General simulation parameters\n##\n'
-	map(print_template, parts.settings.sim_templates)
+	map(print_template, settings.sim_templates)
 	print '\n##\n## Algorithm specific parameters\n##\n'
-	map(print_template, parts.settings.alg_templates)
+	map(print_template, settings.alg_templates)
 	print '\n##\n## Part selection parameters\n##\n'
-	map(print_template, parts.settings.part_templates)
+	map(print_template, settings.part_templates)
 
 
 def arguments_from_templates(parser, templates):
@@ -219,13 +222,13 @@ if __name__=="__main__":
 	run_parser.add_argument('workload', help='The workload file')
 
 	sim_group = run_parser.add_argument_group('General simulation parameters')
-	arguments_from_templates(sim_group, parts.settings.sim_templates)
+	arguments_from_templates(sim_group, settings.sim_templates)
 
 	alg_group = run_parser.add_argument_group('Algorithm specific parameters')
-	arguments_from_templates(alg_group, parts.settings.alg_templates)
+	arguments_from_templates(alg_group, settings.alg_templates)
 
 	part_group = run_parser.add_argument_group('Part selection parameters')
-	arguments_from_templates(part_group, parts.settings.part_templates)
+	arguments_from_templates(part_group, settings.part_templates)
 
 	# config parser
 	config_parser = subparsers.add_parser('config', help='Create configuration')
